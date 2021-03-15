b69a46502f5c93d8bb644a2e8f3a5651584fe7c8
Welisson luca <welissonluca17@gmail.com>
2021-03-14 23:26:05 -0300

    feat: todo update create

--- src/index.js
+++ src/index.js
@@ -31,12 +31,14 @@ app.post('/users',(request, response) => {
   if(userAlredyExists)
     return response.status(400).json({error: "user already exists!"})
   
-    users.push({
-    id: uuidv4, 
-    name,
-    username, 
-    todos: []
-  });
+    const newUser = {
+      id: uuidv4(), 
+      name,
+      username, 
+      todos: []
+
+    }
+    users.push(newUser);
 
   return response.status(201).send("Create User Complete");
 
@@ -54,7 +56,7 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
   const { user }  = request;
   
   const todo = {
-    id: uuidv4,
+    id: uuidv4(),
     title,
     done: false, 
     deadline: new Date(deadline), 
@@ -68,7 +70,21 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params;
+  const {title , deadline } = request.body;
+  const { user }  = request;
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if(!todo)
+    return response.status(404).json({error: "Couldnt find todo with the passed ID"})
+
+  
+   todo.title = title;
+   todo.deadline = new Date(deadline)
+
+
+   return response.json(todo);
+
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
